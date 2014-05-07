6eba73b3f67ba55c26d6335f7ab9f4f60404eac1
Benjamin Lupton <b@lupton.cc>
2014-05-08 00:39:10 +1000

    Corrected description for exec-branch example

--- examples/exec-branch-list.js
+++ examples/exec-branch-list.js
@@ -1,4 +1,4 @@
-// Fetch the cwd's branches, and delete each of them except master from the git remote
+// Fetch the cwd's branches, and convert them into an array
 var Chainy = require('../').extend().require(['exec', 'swap', 'log']);
 Chainy.create()
     .exec('git branch --no-color', {cwd:process.cwd()})
