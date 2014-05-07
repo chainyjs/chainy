8dc686f1c1d6438f6e5dbf2f4e5b2c4baadd8833
Benjamin Lupton <b@lupton.cc>
2014-05-08 02:12:43 +1000

    Added options to coffeekup rendering in ssg example. Fixed data assignment in ssg example

--- examples/ssg.js
+++ examples/ssg.js
@@ -3,13 +3,13 @@ var Chainy = require('../').extend({}).require(['loadFiles', 'writeFiles']);
 
 Chainy.create()
 	.loadFiles('src/documents')
-	.addPlugin('coffeekupFiles', function(files, next){
+	.addPlugin('coffeekupFiles', function(opts, next){
 		Chainy.create().require(['set', 'query', 'map'])
-			.set(files)
+			.set(this.data)
 			.query({path: {$grep: 'src/documents/**/*.html.coffee*'}})
 			.map(function(file, complete){
 				Chainy.create().require(['set', 'coffeekup', 'done'])
-					.set(file.content).coffeekup().done(function(err, result){
+					.set(file.content).coffeekup(opts).done(function(err, result){
 						if (err)  return complete(err, file);
 						file.content = result;
 						return complete(null, file);
