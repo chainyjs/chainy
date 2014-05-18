4a790a28475220265e2d5707f92f79feb4de7d60
Benjamin Lupton <b@lupton.cc>
2014-05-18 21:45:48 +1000

    Lintify. Added freeze. Removed plugins.
    - Removed `.extend` as it could be confused with plugin extensions, now
    you must use `.subclass`
    - Added `.freeze()` to freeze the class from receiving extensions,
    useful for base classes
    - Removed plugins for the purpose of having them in their own repos
    - Updated the require code to no longer include local plugins
    - Removed jshint config in favour of a global one

--- examples/exec-branch-list.js
+++
@@ -1,8 +0,0 @@
-// Fetch the cwd's branches, and convert them into an array
-var Chainy = require('../').extend().require(['exec', 'swap', 'log']);
-Chainy.create()
-    .exec('git branch --no-color')
-    .swap(function(value){
-        return value.replace('*', '').replace(/ |^\s+|\s+$/g, '').split('\n');
-    })
-    .log();
--- test/chainy-test.js
+++ test/chainy-test.js
@@ -1,12 +1,27 @@
 (function(){
+	"use strict";
+
 	// Import
 	var expect = require('chai').expect,
 		joe = require('joe')
 
 	// Test Chainy
 	joe.describe('chainy', function(describe,it){
-		var Chainy = require('../').extend()
-		
+		var Chainy = require('../')
+
+		it("should fail when attempting to extend the base class", function(){
+			var err;
+			try {
+				Chainy.addExtension('oops', 'action', function(){
+					throw new Error('deliberate failure')
+				})
+			}
+			catch (_err) {
+				err = _err;
+			}
+			expect(err && err.message).to.contain('frozen')
+		})
+
 		it("should handle errors gracefully", function(next){
 			Chainy.create()
 				.addExtension('oops', 'action', function(){
@@ -19,10 +34,19 @@
 					return next()
 				})
 		})
-	});
 
-	// Test Plugins
-	['set', 'hasfield', 'map', 'exec', 'uniq'].forEach(function(value){
-		require('./plugins/'+value+'-test')
-	})
+		it("should work well", function(next){
+			Chainy.create()
+				.addExtension('set', 'action', function(data){
+					this.data = data
+				})
+				.set('some data')
+				.done(function(err, chainData){
+					expect(err).to.eql(null)
+					expect(chainData).to.eql('some data')
+					expect(chainData).to.eql(this.data)
+					return next()
+				})
+		})
+	});
 })()
